ddcc16f5e450861ab687379edc660f733b29ca3d
trungthanh324 <136559582+trungthanh324@users.noreply.github.com>
2025-06-19 22:30:45 +0700

    Config/Time-Ping-Server

--- src/ZEN.Startup/Program.cs
+++ src/ZEN.Startup/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddQuartz(q =>
     q.AddTrigger(opts => opts
         .ForJob(jobKey)
         .WithIdentity("PingSelfTrigger")
-        .WithCronSchedule("0 */5 * * * ?")
+        .WithCronSchedule("0 */10 * * * ?")
     );
 });
 
